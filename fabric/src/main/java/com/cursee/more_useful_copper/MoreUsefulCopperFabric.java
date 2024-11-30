789892381ce7550fcfb7d7c9528eb35edfe08094
jason13official <jason13official@gmail.com>
2024-11-30 15:37:55 -0500

    fix some things

--- fabric/src/main/java/com/cursee/more_useful_copper/MoreUsefulCopperFabric.java
+++ fabric/src/main/java/com/cursee/more_useful_copper/MoreUsefulCopperFabric.java
@@ -1,8 +1,12 @@
 package com.cursee.more_useful_copper;
 
 import com.cursee.monolib.core.sailing.Sailing;
+import com.cursee.more_useful_copper.core.entity.CopperGolem;
+import com.cursee.more_useful_copper.core.registry.ModEntityTypesFabric;
+import com.cursee.more_useful_copper.core.registry.ModLootTableModifiers;
 import com.cursee.more_useful_copper.core.registry.RegistryFabric;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 
 public class MoreUsefulCopperFabric implements ModInitializer {
     
@@ -11,5 +15,12 @@ public class MoreUsefulCopperFabric implements ModInitializer {
         MoreUsefulCopper.init();
         Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
         RegistryFabric.register();
+
+        MoreUsefulCopperFabric.registerEntityAttributes();
+        ModLootTableModifiers.modifyLootTables();
+    }
+
+    private static void registerEntityAttributes() {
+        FabricDefaultAttributeRegistry.register(ModEntityTypesFabric.COPPER_GOLEM, CopperGolem.createAttributes());
     }
 }
