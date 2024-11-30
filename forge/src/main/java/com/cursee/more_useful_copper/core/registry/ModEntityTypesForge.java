912bc672c3c71ab78003a3b690406e705a73e86c
jason13official <jason13official@gmail.com>
2024-11-30 16:06:26 -0500

    resource location 2

--- forge/src/main/java/com/cursee/more_useful_copper/core/registry/ModEntityTypesForge.java
+++ forge/src/main/java/com/cursee/more_useful_copper/core/registry/ModEntityTypesForge.java
@@ -2,6 +2,7 @@ package com.cursee.more_useful_copper.core.registry;
 
 import com.cursee.more_useful_copper.Constants;
 import com.cursee.more_useful_copper.core.entity.CopperGolem;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.RegistryObject;
@@ -10,5 +11,5 @@ public class ModEntityTypesForge {
 
     public static void register() {}
 
-    public static final RegistryObject<EntityType<CopperGolem>> COPPER_GOLEM = RegistryForge.registerEntityType("copper_golem", () -> EntityType.Builder.<CopperGolem>of(CopperGolem::new, MobCategory.MISC).sized(1.4f, 2.7f).clientTrackingRange(10).build(Constants.MOD_ID+":copper_golem"));
+    public static final RegistryObject<EntityType<CopperGolem>> COPPER_GOLEM = RegistryForge.registerEntityType("copper_golem", () -> EntityType.Builder.<CopperGolem>of(CopperGolem::new, MobCategory.MISC).sized(1.4f, 2.7f).clientTrackingRange(10).build(new ResourceLocation(Constants.MOD_ID, "copper_golem").toString()));
 }
