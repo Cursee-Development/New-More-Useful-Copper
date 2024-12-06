02dda8a226ebacb114b0fdb35cef7ccebab473e0
jason13official <jason13official@gmail.com>
2024-12-06 14:56:23 -0500

    fix copper chain rendering in fabric

--- fabric/src/main/java/com/cursee/more_useful_copper/core/client/MoreUsefulCopperClientFabric.java
+++ fabric/src/main/java/com/cursee/more_useful_copper/core/client/MoreUsefulCopperClientFabric.java
@@ -3,18 +3,26 @@ package com.cursee.more_useful_copper.core.client;
 import com.cursee.more_useful_copper.core.client.entity.model.CopperGolemModel;
 import com.cursee.more_useful_copper.core.client.entity.renderer.CopperGolemRenderer;
 import com.cursee.more_useful_copper.core.entity.CopperGolem;
+import com.cursee.more_useful_copper.core.registry.ModBlocksFabric;
 import com.cursee.more_useful_copper.core.registry.ModEntityTypesFabric;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.datafix.fixes.EntityElderGuardianSplitFix;
 
 public class MoreUsefulCopperClientFabric implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocksFabric.COPPER_CHAIN, RenderType.cutout());
+
         EntityModelLayerRegistry.registerModelLayer(CopperGolem.LAYER_LOCATION, CopperGolemModel::createBodyLayer);
-        // EntityElderGuardianSplitFix ??? todo: investigate
+
         EntityRendererRegistry.register(ModEntityTypesFabric.COPPER_GOLEM, CopperGolemRenderer::new);
+
+        // EntityElderGuardianSplitFix ??? todo: investigate
     }
 }
